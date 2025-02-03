59daeb868dedb4b5847537256026c8e72693a85e
Nithin Raj <nithinraj9100@gmail.com>
2025-02-03 18:26:40 +0530

    feat: enhance session validation with improved cookie handling and user data retrieval

--- apps/backend/src/modules/auth/index.ts
+++ apps/backend/src/modules/auth/index.ts
@@ -1,15 +1,42 @@
 import { FastifyInstance } from "fastify";
+import prisma from "../utils/prisma";
+import { encodeHexLowerCase } from "@oslojs/encoding";
+import { sha256 } from "@oslojs/crypto/sha2";
 
 export default async function authRoutes(fastify: FastifyInstance) {
   fastify.get("/", async (request, reply) => {
-    const sessionToken = await request.cookies.session_token;
-    console.log("sessionToken is zzzzzzzzz", reply.cookies);
-    console.log("sessionToken is ", sessionToken);
+    const signedSessionToken = request.cookies.session_token;
 
-    if (!sessionToken) {
+    if (!signedSessionToken) {
       return {
         status: "unauthenticated",
         message: "No session token found",
+      };
+    }
+
+    const { valid, value: sessionToken } =
+      fastify.unsignCookie(signedSessionToken);
+
+    if (!valid || !sessionToken) {
+      return {
+        status: "unauthenticated",
+        message: "Invalid session token",
+      };
+    }
+
+    const sessionId = encodeHexLowerCase(
+      sha256(new TextEncoder().encode(sessionToken))
+    );
+
+    const session = await prisma.session.findUnique({
+      where: { id: sessionId },
+      include: { users: true },
+    });
+
+    if (!session || new Date(session.expiresAt) < new Date()) {
+      return {
+        status: "unauthenticated",
+        message: "Invalid session token",
       };
     }
 
@@ -17,6 +44,11 @@ export default async function authRoutes(fastify: FastifyInstance) {
       status: "authenticated",
       session_token: sessionToken,
       message: "Valid session found",
+      user: {
+        id: session.users.id,
+        email: session.users.email,
+        name: session.users.name,
+      },
     };
   });
 }
