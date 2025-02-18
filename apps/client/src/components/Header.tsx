b006e3f50436cc01b20b79a49a043b2f8730a0e7
Nithin Raj <nithinraj9100@gmail.com>
2025-02-18 18:12:58 +0530

    refactor: simplify Header component by removing unused status and console logs

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -2,12 +2,9 @@
 import Navbar from "./Navbar";
 
 const Header = ({ data }: { data: any }) => {
-  const { status, user } = data || {};
+  const { user } = data || {};
   // @ts-ignore
   const userName = user?.name | "Guest";
-
-  console.log("status", status);
-  console.log("user", userName);
 
   return (
     <div>
