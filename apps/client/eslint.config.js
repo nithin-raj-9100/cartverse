048ee599de2d81ea91878ea49e5ccdd0260e2d4a
Nithin Raj <nithinraj9100@gmail.com>
2025-03-05 13:51:53 +0530

    feat: add react-compiler plugin to ESLint config and update project title to CartVerse

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -18,6 +19,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@tanstack/query": pluginQuery,
+      "react-compiler": reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -26,6 +28,8 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "@tanstack/query/exhaustive-deps": "error",
+      "react-compiler/react-compiler": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
-  }
+  },
 );
